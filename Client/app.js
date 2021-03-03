8c7dd217d4b681146d34340cbe6a854993066c7c
deisaacs98 <deisaacs98@gmail.com>
2021-03-03 15:34:42 -0600

    Starting to develop functions in app.js to process http requests.

--- Client/app.js
+++ Client/app.js
@@ -25,3 +25,121 @@
 
     $('#my-form').submit( processForm );
 })(jQuery);
+
+(function($){
+    function processGetMovies( e ){
+        var dict = {
+        	Title : this["title"].value,
+        	Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#get-movies').submit( processGetMovies );
+})(jQuery);
+//This needs to be changed so that it differs from the function above.
+//It should perform the Get(int id) request, so we need to find the id
+//based on the given information. Otherwise, we 
+(function($){
+    function processGetMovie( e ){
+        
+        var dict = {
+        	Title : this["title"].value,
+        	Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#get-movie').submit( processGetMovie );
+})(jQuery);
+
+(function($){
+    function updateMovie( e ){
+        var dict = {
+        	Title : this["title"].value,
+        	Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#update-movie').submit( updateMovie );
+})(jQuery);
+
+(function($){
+    function deleteMovie( e ){
+        var dict = {
+        	Title : this["title"].value,
+        	Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'delete',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#delete-movie').submit( deleteMovie );
+})(jQuery);
+
+
+
